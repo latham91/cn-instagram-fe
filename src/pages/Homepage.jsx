86b4be03ae9a70cf3c9d8901f43995edaa7d7a0b
Aaron Latham <latham91@icloud.com>
2024-02-28 18:36:33 +0000

    removed online users

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useContext } from "react";
+// import { useEffect, useContext } from "react";
 import Container from "../components/Container";
-import OnlineUser from "../components/OnlineUser";
+// import OnlineUser from "../components/OnlineUser";
 import CenterBar from "../components/main/CenterBar";
-import { getOnlineUsers } from "../utils/authFetch";
-import { AuthContext } from "../context/AuthContext";
+// import { getOnlineUsers } from "../utils/authFetch";
+// import { AuthContext } from "../context/AuthContext";
 
 export default function Homepage() {
-    const { onlineUsers, setOnlineUsers } = useContext(AuthContext);
+    // const { onlineUsers, setOnlineUsers } = useContext(AuthContext);
 
-    useEffect(() => {
-        const fetchOnlineUsers = async () => {
-            const data = await getOnlineUsers();
+    // useEffect(() => {
+    //     const fetchOnlineUsers = async () => {
+    //         const data = await getOnlineUsers();
 
-            if (data.success) {
-                setOnlineUsers(data.users);
-            }
-        };
+    //         if (data.success) {
+    //             setOnlineUsers(data.users);
+    //         }
+    //     };
 
-        fetchOnlineUsers();
-    }, [setOnlineUsers]);
+    //     fetchOnlineUsers();
+    // }, [setOnlineUsers]);
 
     return (
         <section className="relative py-10">
             <Container>
-                <div className="absolute flex flex-col items-center justify-center gap-3 top-20 left-18 text-slate-800">
+                {/* <div className="absolute flex flex-col items-center justify-center gap-3 top-20 left-18 text-slate-800">
                     {onlineUsers &&
                         onlineUsers.map((user) => (
                             <div key={user._id}>
                                 <OnlineUser user={user} />
                             </div>
                         ))}
-                </div>
+                </div> */}
                 <div className="grid grid-cols-1 place-items-center">
                     <CenterBar />
                 </div>
