7aa78fc4a093cc298c2be370cd204caa21c9d483
Aaron Latham <latham91@icloud.com>
2024-02-28 17:50:46 +0000

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -64,7 +64,6 @@ function AuthProvider({ children }) {
             setLoading(false);
             setUser(data.user);
             setCredentials({});
-            cookies.set("insta_auth", data.token);
 
             navigate("/");
         } catch (error) {
