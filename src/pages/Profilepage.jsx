d836a664ca894554c09357bf85c4b45648a8b3f0
Aaron Latham <latham91@icloud.com>
2024-02-28 19:31:38 +0000

    update

--- src/pages/Profilepage.jsx
+++ src/pages/Profilepage.jsx
@@ -30,11 +30,7 @@ export default function Profilepage() {
                     Joined on {new Date(userPosts.createdAt).toLocaleDateString()}
                 </div>
                 {userPosts.posts ? (
-                    <div
-                        className={`grid ${
-                            userPosts.posts.length === 1 ? "grid-cols-1" : "grid-cols-2"
-                        } w-full gap-5 my-5 max-h-[600px]`}
-                    >
+                    <div className={`grid grid-cols-2 w-full gap-5 my-5 max-h-[600px]`}>
                         {!userPosts.posts ? (
                             <h1 className="text-3xl font-bold">No posts found</h1>
                         ) : (
